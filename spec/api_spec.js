74c29082e18502bbaebd2fd120d22c4827b1138b
Federico Minzoni <fminzoni@enter.it>
2017-03-20 15:28:48 +0100

    add first unit test

---
+++ spec/api_spec.js
@@ -0,0 +1,18 @@
+var request = require("request");
+var config = require('dotenv').config({path: '.env.ci'})
+
+var app = require("../../build/app/app.js")
+
+var base_url = "http://localhost:3001/"
+
+describe("gateway", function() {
+  describe("GET default route /", function() {
+    it("returns status code 200", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+      
+    });
+  });
+});
